6de9e28733f7ed0d0e7d9abafadb264040e5ee13
Masaki <82437472+Masaki-Nishimoto@users.noreply.github.com>
2022-06-29 14:18:18 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// buttonSet1
+$( function() {
+
+  $( '#buttonTwitter' ).click( function() {
+    $( 'h1' ).html( '<p><a href="https://instagram.com/93kashii_fes?igshi=NDBIY2NjN2I=">香椎祭公式アカウント＠九州産業大学</a></p>' );
+  });
+
+  $( '#buttonInstagram' ).click( function() {
+    $( 'h1' ).html( '<p><a href="https://twitter.com/kashiifes?s=11&t=IWAuLzdoN-9yh7zb3wTyuQ">九州産業大学　香椎祭</a></p>' );
+  });
+
+  $( '#buttonYouTube' ).click( function() {
+    $( 'h1' ).html( '<p><a href="https://www.youtube.com/channel/UCx-8vbSgYjTmTIHn4D8qcXQ">九産大　香椎祭ちゃんねる</a></p>' );
+  });
+
+  $( '#buttonTiktok' ).click( function() {
+    $( 'h1' ).html( '<p><a href="https://vt.tiktok.com/ZSdKDX6Md/">九州産業大学　大学祭常任中央実行委員会</a></p>' );
+  });
+
+});
